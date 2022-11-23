3b3be60c0f2a7bdae0d32dbdba92cb578b39e880
Marc Melià <marcmelia10@gmail.com>
2022-11-23 15:45:31 +0100

    canvis a missatge.php

---
+++ BD249608499Y/missatge.php
@@ -0,0 +1,37 @@
+<?php
+include('../conexion.php');
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../lib/app.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <header>
+        <?php include('../header.php'); ?>
+    </header>
+    <main>
+      
+
+        <br><input type="text" id="fname" name="fname"><br>
+
+    </main>
+    <footer>
+        <?php include('../footer.php') ?>
+    </footer>
+</body>
+
+</html>
+
